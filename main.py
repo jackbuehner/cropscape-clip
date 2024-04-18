e9553c2a1b50179b3352932ce8c24b6dc7e15b42
Jack Buehner <jackb@furman.edu>
2024-04-18 12:48:36 -0400

    feat: add function to consolidate pixel classes for cropland data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from clip_cropscape_to_area_of_interest import clip_cropscape_to_area_of_interest
+from consolidate_cropland import consilidate_all_cropland
 
 # limit to our area of interest by clipping first, which will also make subsequent steps faster
 clip_cropscape_to_area_of_interest('/input', '/input/area_of_interest.shp', '/working/clipped')
+
+# consolidate similar pixel classes, inlcuing all cropland classes
+consilidate_all_cropland('/working/clipped', '/output')
