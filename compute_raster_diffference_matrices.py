691d3351dec47969c632db22d52f60fdcac2d9f4
Jack Buehner <jackb@furman.edu>
2024-05-13 11:50:05 -0400

    feat: also calculate difference matrices for 3-year, 5-year, and all-year gaps

--- compute_raster_diffference_matrices.py
+++ compute_raster_diffference_matrices.py
@@ -52,50 +52,58 @@ def compute_raster_difference_matrices(rasters_list: list[tuple[str, int]], recl
   # loop through the rasters and calculate a difference proprtion matrix for each pair of years
   diff_matrices_labels: list[str] = ['Legend']
   diff_matrices: list[pandas.DataFrame] = [pairs_dataframe]
-  for (index, (file_this_year, year)) in enumerate(rasters_list):
-    # get data for the previous year
-    file_last_year, last_year = next((t for t in rasters_list if t[1] == year - 1), (None, None))
-    
-    # cancel this iteration if there is no previous year available
-    if file_last_year is None or last_year is None: continue
-    
-    # calculate the change in pixel classes between the previous year and the current year
-    if status: status.update(f'{status_prefix}Computing difference between {last_year} and {year}...')
-    diff_array = compute_raster_class_difference(
-      file_last_year,
-      file_this_year,
-      diff_specs,
-      f'./TEMPORARY/class_diffs/{last_year}_{year}.tiff'
-    )
-    if status: status.console.log(f'{status_prefix}Difference computed ({last_year} → {year})')
-    
-    # calculate summary metadata (pixel counts) for the rasters
-    # so we can calculate proportions of change later
-    from_metadata = summarize_raster(file_last_year)
-    diff_metadata = summarize_raster(f'./TEMPORARY/class_diffs/{last_year}_{year}.tiff')
-    if status: status.console.log(f'{status_prefix}Metadata summarized ({last_year} → {year})')
-    
-    # clean up temporary folder
-    shutil.rmtree('./TEMPORARY')
-    
-    # build a list of proportions of change for every possible pair of classes
-    diff_proportions_list = []
-    for (from_id, from_name), (to_id, to_name) in itertools.product(classes, repeat=2):
-      # get the spec, which contains the from and to pixel values
-      diff_id, spec = next(((key, value) for (key, value) in diff_specs.items() if value['name'] == f'{from_name} → {to_name}'), (None, { 'from': [None] }))
-
-      # get the counts and calculate a proportion
-      from_count = from_metadata['pixel_counts'].get(spec['from'][0], 0)
-      diff_count = diff_metadata['pixel_counts'].get(diff_id, 0)
-      diff_proportion = 0 if from_count == 0 else diff_count / from_count
-      diff_proportions_list.append(f'{diff_proportion:.4f}')
-    
-    # build a dataframe showing the proportion change/diff for each possible pair of classes
-    diff_proportions_array = numpy.array(diff_proportions_list)
-    diff_proportions_matrix = numpy.matrix(diff_proportions_array.reshape(classes_length, classes_length))
-    diff_proportions_dataframe = df=pandas.DataFrame(diff_proportions_matrix, columns=classes, index=classes)
-    diff_matrices_labels.append(f'{last_year} → {year}')
-    diff_matrices.append(diff_proportions_dataframe)
-    if status: status.console.log(f'{status_prefix}Difference proportions matrix calculated ({last_year} → {year})')
-    
+  
+  # function to calculate difference matrix for a given year gap
+  def calculate_difference_matrix(year_gap: int) -> None:
+    for (index, (file_this_year, year)) in enumerate(rasters_list):
+      # get data for the previous year
+      file_last_year, last_year = next((t for t in rasters_list if t[1] == year - year_gap), (None, None))
+      
+      # cancel this iteration if there is no previous year available
+      if file_last_year is None or last_year is None: continue
+      
+      # calculate the change in pixel classes between the previous year and the current year
+      if status: status.update(f'{status_prefix}Computing difference between {last_year} and {year}...')
+      diff_array = compute_raster_class_difference(
+        file_last_year,
+        file_this_year,
+        diff_specs,
+        f'./TEMPORARY/class_diffs/{last_year}_{year}.tiff'
+      )
+      if status: status.console.log(f'{status_prefix}Difference computed ({last_year} → {year})')
+      
+      # calculate summary metadata (pixel counts) for the rasters
+      # so we can calculate proportions of change later
+      from_metadata = summarize_raster(file_last_year)
+      diff_metadata = summarize_raster(f'./TEMPORARY/class_diffs/{last_year}_{year}.tiff')
+      if status: status.console.log(f'{status_prefix}Metadata summarized ({last_year} → {year})')
+      
+      # clean up temporary folder
+      shutil.rmtree('./TEMPORARY')
+      
+      # build a list of proportions of change for every possible pair of classes
+      diff_proportions_list = []
+      for (from_id, from_name), (to_id, to_name) in itertools.product(classes, repeat=2):
+        # get the spec, which contains the from and to pixel values
+        diff_id, spec = next(((key, value) for (key, value) in diff_specs.items() if value['name'] == f'{from_name} → {to_name}'), (None, { 'from': [None] }))
+        # get the counts and calculate a proportion
+        from_count = from_metadata['pixel_counts'].get(spec['from'][0], 0)
+        diff_count = diff_metadata['pixel_counts'].get(diff_id, 0)
+        diff_proportion = 0 if from_count == 0 else diff_count / from_count
+        diff_proportions_list.append(f'{diff_proportion:.4f}')
+      
+      # build a dataframe showing the proportion change/diff for each possible pair of classes
+      diff_proportions_array = numpy.array(diff_proportions_list)
+      diff_proportions_matrix = numpy.matrix(diff_proportions_array.reshape(classes_length, classes_length))
+      diff_proportions_dataframe = df=pandas.DataFrame(diff_proportions_matrix, columns=classes, index=classes)
+      diff_matrices_labels.append(f'{last_year} → {year}')
+      diff_matrices.append(diff_proportions_dataframe)
+      if status: status.console.log(f'{status_prefix}Difference proportions matrix calculated ({last_year} → {year})')
+  
+  # calculate difference matrices for different year gaps
+  calculate_difference_matrix(1)  # one year gap
+  calculate_difference_matrix(3)  # three year gap
+  calculate_difference_matrix(5)  # five year gap
+  calculate_difference_matrix(len(rasters_list) - 1)  # entire year range
+  
   return (diff_matrices_labels, diff_matrices)
